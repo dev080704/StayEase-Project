c27cb37049a784855282ad6e426f9f83695cea9d
Manan Sharma <manan9932@yahoo.com>
2025-04-22 13:52:33 +0530

    Resolved merge conflicts in routes/listing.js and models/user.js

--- routes/listing.js
+++ routes/listing.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require("express");
 const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
@@ -28,41 +27,4 @@ router
 
 //Edit Route
 router.get("/:id/edit", isLoggedIn, isOwner, wrapAsync (listingController.renderEditForm));
-
-
-
-=======
-const express = require("express");
-const router = express.Router();
-const wrapAsync = require("../utils/wrapAsync.js");
-const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
-const listingController = require("../controllers/listing.js");
-const multer  = require('multer');
-const { storage } = require("../cloudConfig.js");
-const upload = multer({ storage });
-
-router
-    .route("/") 
-    .get(wrapAsync (listingController.index))
-    .post(isLoggedIn, upload.single('listing[image]'), validateListing, wrapAsync (listingController.createListing)
-);
-
-//New Route
-router.get("/new", isLoggedIn, listingController.renderNewForm);
-
-
-router
-    .route("/:id")
-    .get(wrapAsync (listingController.showListing))
-    .put(isLoggedIn, isOwner, upload.single('listing[image]'), validateListing, wrapAsync (listingController.updateListing))
-    .delete(isLoggedIn, isOwner, wrapAsync (listingController.deleteListing)
-);
-
-
-//Edit Route
-router.get("/:id/edit", isLoggedIn, isOwner, wrapAsync (listingController.renderEditForm));
-
-
-
->>>>>>> e7365edfee5f6e1e8b983133e05e7b3a72ae3902
 module.exports = router;
