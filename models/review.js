f54952ea1ba01a4aaa49b796d5b5d94dade27b35
Manan Sharma <manan9932@yahoo.com>
2025-04-22 14:09:44 +0530

    Resolved merge conflict in review.js

--- models/review.js
+++ models/review.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
@@ -19,26 +19,5 @@ const reviewSchema = new Schema({
     },
 });
 
-=======
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
 
-const reviewSchema = new Schema({
-    comment: String,
-    rating: {
-        type: Number,
-        min: 1,
-        max: 5
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now()
-    },
-    author: {
-        type: Schema.Types.ObjectId,
-        ref: "User",
-    },
-});
-
->>>>>>> e7365edfee5f6e1e8b983133e05e7b3a72ae3902
 module.exports = mongoose.model("Review", reviewSchema);
