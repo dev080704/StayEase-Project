89859294726e53644f1e2279a9344187b7d9294e
Manan Sharma <manan9932@yahoo.com>
2025-04-22 13:45:39 +0530

    Fix: resolve conflict in user.js

--- models/user.js
+++ models/user.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const passportLocalMongoose = require("passport-local-mongoose");
@@ -12,19 +12,6 @@ const userSchema = new Schema ({
 
 userSchema.plugin(passportLocalMongoose);
 
-=======
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-const passportLocalMongoose = require("passport-local-mongoose");
 
-const userSchema = new Schema ({
-    email: {
-        type: String,
-        required: true
-    }
-});
 
-userSchema.plugin(passportLocalMongoose);
-
->>>>>>> e7365edfee5f6e1e8b983133e05e7b3a72ae3902
 module.exports = mongoose.model("User", userSchema);
